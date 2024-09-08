6c0fe35c265d08aec733d011f8fc39d830a1e66e
Dawit <davedawita@yahoo.com>
2024-09-08 03:03:08 -0400

    styling projects list

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,40 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 function Projects(props) {
   // create state to hold projects
-  const [projects, setProjects] = useState(null);
+  const [projects, setProjects] = useState(null)
 
   //create function to make api call
   const getProjectsData = async () => {
     //make api call and get response
-    const response = await fetch(props.URL + "projects");
+    const response = await fetch(props.URL + "projects")
     // turn response into javascript object
     const data = await response.json();
     // set the projects state to the data
-    setProjects(data);
-  };
+    setProjects(data)
+  }
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => {getProjectsData()}, []);
+  useEffect(() => {getProjectsData()}, [])
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
-      <div className="projectlist">
-        <h1 className="welcome">Project Name: {project.name}</h1>
-        <div className="projectimage"><img src={project.image} /></div>
-        <a href={project.gitFrontEnd}>
-          <button>Github-Front-End</button>
-        </a>
-        <a href={project.gitBackEnd}>
-          <button>Github-Back-End</button>
-        </a>
-        <a href={project.live}>
-          <b><button>live site</button></b>
-        </a>
-      </div>
-    ));
-  };
+      
+        <div className="projectlist">          
+            <div className="projectimage">
+            <h1 className="welcome">Project Name: {project.name}</h1>
+              <img style={{ width: 800, height: 400 }} src={project.image} alt="image" />
+                <div className="projlinks">
+                  <a href={project.gitFrontEnd}>
+                  <button>Github-Front-End</button>
+                  </a>
+                  <a href={project.gitBackEnd}>
+                    <button>Github-Back-End</button>
+                  </a>
+                  <a href={project.live}>
+                  <b><button>live site</button></b>
+                  </a>
+                </div>
+              
+            </div>
+          
+        </div>
+      
+      
+    ))
+  }
 
   return projects ? loaded() : <h1>Loading...</h1>;
 }
